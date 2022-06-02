dd28f6aadcd4f28f788847b5f81ec618e3d22df5
Stephen McLean <stephenmclean@Stephens-MacBook-Pro-2.local>
2022-06-02 09:26:19 +0100

    fix colpick import issue

--- index.js
+++ index.js
@@ -12,13 +12,13 @@ module.exports = {
   blueprintsPath: function() {
     return path.join(__dirname, 'blueprints');
   },
-  included: function colpick_included() {
+  included: function colpick_included(app) {
     this._super.included.apply(this, arguments);
     if(!process.env.EMBER_CLI_FASTBOOT) {
       var colpickPath = path.join('node_modules', 'jquery-colpick');
 
-      this.app.import(path.join(colpickPath, 'js',  'colpick.js'));
-      this.app.import(path.join(colpickPath, 'css', 'colpick.css'));
+      app.import(path.join(colpickPath, 'js',  'colpick.js'));
+      app.import(path.join(colpickPath, 'css', 'colpick.css'));
     }
   }
 };
